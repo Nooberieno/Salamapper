8b9711e401c66a2ea99980bd40e03f8f23b897a8
Nooberieno <nooberieno.dev@gmail.com>
2024-04-29 18:11:07 +0200

    first working prototype with the c++ default unordered_map.

--- salamapper.cpp
+++ salamapper.cpp
@@ -6,19 +6,20 @@
 #include "TreeNode.h"
 #include "rooter.h"
 
-void PrintTree(TreeNode* node, std::string& prefix, bool last = true, bool first = true){
-	if(node->parent != nullptr){
-		std::cout<< prefix << (last? "└─" : "├─") << node->id <<std::endl;
-	}else{
-		std::cout<< prefix << node->id << std::endl;
-	}
-	size_t original_length = prefix.length();
-	first? (prefix += last? "    " : "│  ") : (prefix += last? "    " : "│  ");
-	size_t children_count = node->children.size();
-	for(size_t i = 0; i < children_count; i++){
-		PrintTree(node->children[i], prefix, i == children_count - 1, node->parent == nullptr);
-	}
-	prefix.erase(original_length);
+void PrintTree(TreeNode* node, std::string& prefix, bool last = true, bool first = true) {
+    if (node->parent != nullptr) {
+        std::cout << prefix << (last ? "└─" : "├─") << node->id << std::endl;
+    } else {
+        std::cout << prefix << node->id << std::endl;
+    }
+    size_t original_length = prefix.length();
+    first ? (prefix += last ? "" : "│  ") : (prefix += last ? "    " : "│  ");
+    
+    size_t children_count = node->children.size();
+    for (size_t i = 0; i < children_count; i++) {
+        PrintTree(node->children[i], prefix, i == children_count - 1, false);
+    }
+    prefix.erase(original_length);
 }
 
 std::unordered_map<std::string, std::vector<std::string>> g = {
