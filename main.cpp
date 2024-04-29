48b9d472c2454e48b30d72983e022aa37128f44c
Nooberieno <nooberieno.dev@gmail.com>
2024-04-29 17:16:23 +0200

    Implementation for the BuildTree function

--- main.cpp
+++ main.cpp
@@ -21,7 +21,15 @@ class TreeNode{
 };
 
 TreeNode* BuildTree(const std::unordered_map<std::string, std::vector<std::string>>& g, TreeNode* node, TreeNode* parent){
-	//turn into rooted treegraph
+	for(const auto& child_id : g.at(node->id)){
+		if(parent != nullptr && child_id == parent->id){
+			continue;
+		}
+		TreeNode* child = new TreeNode(child_id, parent);
+		node->children.push_back(child);
+		BuildTree(g, child, node);
+	}
+	return node;
 }
 
 TreeNode* RootTree(const std::unordered_map<std::string, std::vector<std::string>>&g, const std::string& root_id){
