ebecc88b4f391ee8ace88dd2aedb5228f9e3dd47
Nooberieno <nooberieno.dev@gmail.com>
2024-10-23 22:41:17 +0200

    C api test, non functional, does not compile

---
+++ tests/C_api_test.c
@@ -0,0 +1,39 @@
+#include <salamapper/salamapper.h>
+#include <assert.h>
+#include <stdio.h>
+
+void BasicTest(){
+	const char* input = "1:2,3,5;2:3;3:4;4:;5:6,7;6:7;7:;";
+	const char* root_id = "1";
+
+	TreeHandle* tree = CreateTree(input, root_id);
+	assert(tree != NULL && "Tree Creation Failed");
+
+	char* ASCII_Output = GetTree(tree);
+	assert(ASCII_Output != NULL && "ASCII_Output Failed");
+	printf("Tree visualization: \n%s\n", ASCII_Output);
+
+	FreeString(ASCII_Output);
+	ClearTree(tree);
+	printf("Basic functionality test passed");
+}
+
+void ErrorHandlingTest(){
+	TreeHandle *tree = CreateTree("invalid format", "1");
+	assert(tree == NULL && "Invalid input handling failed");
+
+	tree = CreateTree(NULL, "1");
+	assert(tree == NULL && "Null input handling failed");
+
+	printf("Error handling test passed");
+}
+
+int main(){
+	printf("Starting salamapper C API test... \n");
+
+	BasicTest();
+	ErrorHandlingTest();
+
+	printf("All test completed succesfully");
+	return 0;
+}
